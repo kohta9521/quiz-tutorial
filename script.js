18cf1ca8004280f5afb85858f746bbdf3292f769
kohta.k <kohta0421kochi@gmail.com>
2022-07-12 10:11:12 +0900

    javasuript　記述　問題文部分

--- script.js
+++ script.js
@@ -2,3 +2,46 @@ const quizContainer = document.getElementById('quiz');
 const resultContainer = document.getElementById('result');
 const submitButton = o\document.getElementById('submit');
 
+function buildQuiz() {}
+
+function showResults() {}
+
+
+const myQuestion = [
+    {
+        question: "Who is the strongest?",
+        answers: {
+            a: "Superman",
+            b: "The Terminator",
+            c: "Waluigi, obviously"
+        },
+        correctAnswe: "c"
+    },
+    {
+        question: "What is the best site ever created?",
+        answers: {
+            a: "SitePoint",
+            b: "Simple Steps Code",
+            c: "Trick question; they're both the best"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where is Waldo really?",
+        answers: {
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+    }
+]
+
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
+
