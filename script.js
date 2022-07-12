33e37d504155b4093f1f361dd2c0e41516b13256
kohta.k <kohta0421kochi@gmail.com>
2022-07-12 10:34:35 +0900

    エラー回避

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 const quizContainer = document.getElementById('quiz');
 const resultContainer = document.getElementById('result');
-const submitButton = o\document.getElementById('submit');
+const submitButton = document.getElementById('submit');
 
-function buildQuiz() {}
+function buildQuiz() {
+    const output = [];
 
-function showResults() {}
+    myQuestion.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for(letter in currentQuestion.answes){
+                answers.push(
+                    `<label>
+                    <input type="radio"  name="question${questionNumber}" value="${letter}">
+                    ${letter}:
+                    ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')}</div>`
+            );
+        }
+    );
+
+    quizContainer.innerHTML = output.join("");
+}
+
+function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    myQuestion.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = 'input[name=question' + questionNumber+']:checked';
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if(userAnswer===currentQuestion.correctAnswer) {
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else {
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    resultContainer.innerHTML = numCorrect + 'out of' + myQuestion.length;
+}
 
 
 const myQuestion = [
